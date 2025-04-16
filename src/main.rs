9d1ba28f2b407b6f82121664f0153215c8472203
James Benner <james.benner@gmail.com>
2025-04-16 19:53:09 -0400

    Replace unwrap and update return type

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 use itertools::Itertools;
 use std::env;
+use std::io::Error;
 use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     let path_name = if args.len() > 1 { &args[1] } else { "." };
 
     Path::new(path_name)
-        .read_dir()
-        .unwrap()
+        .read_dir()?
         .filter(|dir_entry| dir_entry.as_ref().unwrap().file_type().unwrap().is_dir())
         .map(|dir_entry| dir_entry.unwrap().file_name())
         .sorted()
-        .for_each(|dir| println!("{}", dir.to_str().unwrap()))
+        .for_each(|dir| println!("{}", dir.to_str().unwrap()));
+
+    Ok(())
 }
